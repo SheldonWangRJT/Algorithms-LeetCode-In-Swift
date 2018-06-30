d30e85b0d3dcc15290aff5652b053c365d444c75
Sheldon Wang <daniel.wangxiao@gmail.com>
2018-06-30 17:15:13 -0500

    Added accounts merge

--- Algorithms.swift
+++ Algorithms.swift
@@ -1722,4 +1722,81 @@ class Algorithms: NSObject {
         }
         return j == 1
     }
+    
+    //721. Accounts Merge
+    /**
+     Given a list accounts, each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+     
+     Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some email that is common to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+     
+     After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+     
+     Example 1:
+     Input:
+     accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
+     Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],  ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
+     Explanation:
+     The first and third John's are the same person as they have the common email "johnsmith@mail.com".
+     The second John and Mary are different people as none of their email addresses are used by other accounts.
+     We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'],
+     ['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+     Note:
+     
+     The length of accounts will be in the range [1, 1000].
+     The length of accounts[i] will be in the range [1, 10].
+     The length of accounts[i][j] will be in the range [1, 30].
+     */
+    
+    var graph = [String: [String]]()
+    var visited = Set<String>()
+    
+    func accountsMerge(_ accounts: [[String]]) -> [[String]] {
+        guard !accounts.isEmpty else { return [] }
+        var names = [String: String]()
+        for account in accounts {
+            for email in Array(account[1...]) {
+                if let v = graph[account[1]] {
+                    if !v.contains(email) {
+                        graph[account[1]] = v + [email]
+                    }
+                } else {
+                    graph[account[1]] = [email]
+                }
+                if let v = graph[email] {
+                    if !v.contains(account[1]) {
+                        graph[email] = v + [account[1]]
+                    }
+                } else {
+                    graph[email] = [account[1]]
+                }
+                names[email] = account[0]
+            }
+        }
+        
+        var groups = [[String]]()
+        for account in accounts {
+            if !visited.contains(account[1]) {
+                let group = addToList(account[1], [String]())
+                groups.append(group)
+            }
+        }
+        
+        var res = [[String]]()
+        for group in groups {
+            guard let name = names[group[0]] else { continue }
+            res.append([name]+group.sorted())
+        }
+        return res
+    }
+    
+    private func addToList(_ email: String, _ list: [String]) -> [String] {
+        if visited.contains(email) { return list }
+        guard let emails = graph[email], !emails.isEmpty else { return list }
+        visited.insert(email)
+        var res = list + [email]
+        for item in emails {
+            res += addToList(item, list)
+        }
+        return res
+    }
 }
